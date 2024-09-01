8fe490006678de3795b47f4ec0e57e6d8a33f478
mayasato0401 <maya.s0401.wdr@gmail.com>
2024-09-01 20:30:20 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kadai_007</title>
+</head>
+<body>
+    <p>
+        <?php
+        // 連想配列
+        $user_date = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+        // 値を出力する
+        print_r($user_date);
+        ?>
+    </p>
+</body>
+</html>
